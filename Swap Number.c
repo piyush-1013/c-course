512887c53bf28355818464bd7d4ec6c53c3f3084
piyush-1013 <piyush.magdum10@gmail.com>
2026-03-30 10:21:02 +0530

    Swap Number.c

---
+++ Swap Number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+  int a, b, c;
+  printf("Please Enter first number: "); 
+  scanf("%d", &a);  
+  printf("\nPlease Enter second number: "); 
+  scanf("%d", &b);  
+  printf("Current Value Of variable 1 is: %d", a);
+  printf("\nCurrent Value Of variable 2 is: %d", b);  
+  c = a;
+  a = b;
+  b = c;
+  printf("\nCurrent Value Of variable 1 is: %d", a);
+  printf("\nCurrent Value Of variable 2 is: %d", b);     
+  return 0; 
+} 
