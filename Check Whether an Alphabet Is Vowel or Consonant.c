0464297a76e7606a4859e6c83a0a9b754b421516
piyush-1013 <piyush.magdum10@gmail.com>
2026-03-30 10:28:53 +0530

    Check Whether an Alphabet Is Vowel or Consonant.c

---
+++ Check Whether an Alphabet Is Vowel or Consonant.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+int main(int argc, char const *argv[])
+{
+  char ch;
+  printf("Please Enter a Character: ");
+  scanf("%ch", &ch);
+  if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+  {
+    printf("The Character is Vowel");
+  }
+  else
+  {
+    printf("The Charcter is Consonant");
+  }
+  return 0;
+} 
